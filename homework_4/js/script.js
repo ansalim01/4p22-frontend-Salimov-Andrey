cda2ecbcf78a3ee5fd4c7095d76ca894f8b05112
ansalim01 <ansalim14@gmail.com>
2022-09-29 02:06:26 +0300

    test-ret

---
+++ homework_4/js/script.js
@@ -0,0 +1,21 @@
+'use strict'
+let b= 1.005+Number.EPSILON
+let a = Math.round(b*100)/100
+console.log(a);
+
+
+let value="135.58px"
+
+console.log(parseFloat(value));
+
+
+let c =50+"text";
+if(isNaN(c)){
+    console.log('рузултата NAN');
+}
+
+console.log(Math.max(10,58,39,-150,0));
+
+
+
+console.log(Math.floor(58.858));
